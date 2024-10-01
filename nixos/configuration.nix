ddbe145e41a257d3d30311e8cfc8c094abdc98e2
Sigge Rajamäe <siggerajamae@gmail.com>
2024-10-02 01:42:27 +0200

    After install

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,96 +1,104 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-    imports = [
-        /etc/nixos/hardware-configuration.nix # Generated hardware configuration
-        ./system.nix # Additional system specific configuration
-    ];
+  imports = [
+    /etc/nixos/hardware-configuration.nix # Generated hardware configuration
+    ./system.nix # Additional system specific configuration
+  ];
 
-    # Use the systemd-boot EFI boot loader
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  # Use the systemd-boot EFI boot loader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
-    # Console keyboard layout
-    console.keyMap = "sv-latin1";
+  # Console keyboard layout
+  console.keyMap = "sv-latin1";
 
-    # NetworkManager
-    networking.networkmanager.enable = true;
+  # NetworkManager
+  networking.networkmanager.enable = true;
 
-    # Time zone
-    time.timeZone = "Europe/Stockholm";
+  # Time zone
+  time.timeZone = "Europe/Stockholm";
 
-    # Pipewire
-    services.pipewire = {
-        enable = true;
-        pulse.enable = true;
-    };
+  # Pipewire
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
 
-    # Touchpad
-    services.libinput.enable = true;
+  # Touchpad
+  services.libinput.enable = true;
 
-    # Users
-    users.users.sigge = {
-        isNormalUser = true;
-        extraGroups = [ "wheel" "networkmanager" ];
-    };
+  # Users
+  users.users.sigge = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
 
-    # Enable GNOME Keyring
-    services.gnome.gnome-keyring.enable = true;
+  # Enable GNOME Keyring
+  services.gnome.gnome-keyring.enable = true;
 
-    # No sudo password prompt
-    security.sudo.extraRules = [
+  # No sudo password prompt
+  security.sudo.extraRules = [
+    {
+      users = [ "sigge" ];
+      commands = [
         {
-            users = ["sigge"];
-            commands = [
-                {
-                    command = "ALL";
-                    options = ["NOPASSWD"];
-                }
-            ];
+          command = "ALL";
+          options = [ "NOPASSWD" ];
         }
-    ];
+      ];
+    }
+  ];
 
-    # System packages
-    environment.systemPackages = with pkgs; [
-        neovim
-        wget
-        firefox
-        tree
-        sway
-        wl-clipboard
-        grim
-        bemenu
-        alacritty
-        git
-        cargo
-        clang
-        libva-utils
-        fzf
-        fd
-        unzip
-        libva-utils
-        github-cli
-        vlc
-        htop
-        nnn
-    ];
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-    # Fonts
-    fonts.packages = 
-        let 
-            nerdfonts = pkgs.nerdfonts.override {
-                fonts = [
-                    "NerdFontsSymbolsOnly"
-                    "CommitMono"
-                ];
-            };
-        in with pkgs; [
-            nerdfonts
+  # System packages
+  environment.systemPackages = with pkgs; [
+    alacritty
+    bemenu
+    cargo
+    clang
+    discord
+    fd
+    firefox
+    fzf
+    git
+    github-cli
+    grim
+    htop
+    libva-utils
+    neovim
+    nixpkgs-fmt
+    nnn
+    pavucontrol
+    spotify
+    sway
+    trash-cli
+    tree
+    unzip
+    vlc
+    wget
+    wl-clipboard
+  ];
+
+  # Fonts
+  fonts.packages =
+    let
+      nerdfonts = pkgs.nerdfonts.override {
+        fonts = [
+          "NerdFontsSymbolsOnly"
+          "CommitMono"
         ];
+      };
+    in
+    with pkgs; [
+      nerdfonts
+    ];
 
-    # Enable Sway
-    programs.sway = {
-        enable = true;
-        wrapperFeatures.gtk = true;
-    };
+  # Enable Sway
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true;
+  };
 }
